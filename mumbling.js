1720cce5e47ce02d4487a1c744ea62ac465d1787
mattdamon108 <woonki.moon@gmail.com>
2019-07-06 20:57:15 +0900

    [CODEWAR] mumbling

---
+++ mumbling.js
@@ -0,0 +1,24 @@
+function main() {
+  const sample1 = "accum";
+  const result = mumbling(sample1);
+  console.log(result);
+}
+
+/* Mumbling
+1. Split word by each idiom
+2. Make an array with the idiom and length of index
+3. Join it to word and push it to accumulator in reduce func, then return the result after join it with "-"
+*/
+
+function mumbling(s) {
+  return s
+    .split("")
+    .reduce((acc, cur, i) => {
+      let mumbled = Array(i + 1).fill(cur.toLowerCase());
+      mumbled[0] = mumbled[0].toUpperCase();
+      return [...acc, mumbled.join("")];
+    }, [])
+    .join("-");
+}
+
+main();
