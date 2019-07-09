e88ea8ebef3ad61b77ca39194c581d34bc29f492
mattdamon108 <woonki.moon@gmail.com>
2019-07-10 00:12:44 +0900

    [CODEWAR] currying - remove using reduce

--- currying.js
+++ currying.js
@@ -1,18 +1,18 @@
 /* Currying
-1. Firstly, I tried to implement it with recursive function, but there's a problem to compute args.
+1. Firstly, I tried to implement it with recursive closure, but there's a problem to compute args.
 2. I searched how to implement and compute in currying.
 3. Check `Object.valueOf` method to return primitive value of object
  */
 
 // TODO: check the testing cases
 function add(n) {
-  const nums = [];
+  let sum = n;
   const currying = m => {
-    nums.push(m);
+    sum += m;
     return currying;
   };
 
-  currying.valueOf = () => nums.reduce((acc, cur) => acc + cur, n);
+  currying.valueOf = () => sum;
 
   return currying;
 }
