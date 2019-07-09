6a2b54657bf085719bee03bdfe138480e5039c22
mattdamon108 <woonki.moon@gmail.com>
2019-07-10 00:10:14 +0900

    [CODEWAR] currying - fix test case & add comment

--- currying.js
+++ currying.js
@@ -1,4 +1,7 @@
-/*
+/* Currying
+1. Firstly, I tried to implement it with recursive function, but there's a problem to compute args.
+2. I searched how to implement and compute in currying.
+3. Check `Object.valueOf` method to return primitive value of object
  */
 
 // TODO: check the testing cases
--- test/currying.test.js
+++ test/currying.test.js
@@ -2,14 +2,18 @@ const answer = require("../currying");
 
 describe("A Currying", () => {
   it("TEST: add(2) is equal to 2", () => {
-    expect(answer.add(2)).toBe(2);
+    expect(answer.add(2) == 2);
   });
 
   it("TEST: add(2)(3) is equal to 5", () => {
-    expect(answer.add(2)(3)).toBe(5);
+    expect(answer.add(2)(3) == 5);
   });
 
   it("TEST: add(2)(3)(4) is equal to 9", () => {
-    expect(answer.add(2)(3)(4)).toBe(9);
+    expect(answer.add(2)(3)(4) == 9);
+  });
+
+  it("TEST: add(2)(3) + 5 is equal to 10", () => {
+    expect(answer.add(2)(3) + 5 === 10);
   });
 });
